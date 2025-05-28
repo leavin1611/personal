b6f46f7c6de27772cd772cb30df8ff006a0cc22e
leavin1611 <leavin1611@gmail.com>
2025-05-28 12:01:50 +0530

    Add files via upload

---
+++ web_modal_lab2/calc.js
@@ -0,0 +1,7 @@
+ const btn= document.querySelector(".btn");
+  const res = document.getElementById("#a");
+btn.onclick = () => {
+    let ans=document.getElementsByClassName(".quan") * document.getElementsByClassName(".price1");
+
+    document.getElementById("a").innerHTML = "2";
+}
